e7dc535462335ac52f5268182e1af4bc7545778f
badsyntaxx <badsyntaxx@gmail.com>
2024-06-11 15:54:53 -0600

    updates

--- nuvia/menu.ps1
+++ nuvia/menu.ps1
@@ -1,18 +1,12 @@
 function menu {
     try {
-        clear-host
-        write-welcome -Title "Nuvia Menu" -Description "Select an action to take." -Command "menu"
-
-        $url = "https://raw.githubusercontent.com/badsyntaxx/chased-intech-scripts/main"
-
-        write-text -Type "header" -Text "Selection" -LineAfter -LineBefore
         $choice = read-option -options $([ordered]@{
                 "Install TScan"     = "Install TScan software."
                 "ISR Onboard"       = "Collection of functions to onboard and ISR computer."
                 "ISR Install Apps"  = "Install all the apps an ISR needs to work."
                 "ISR Install Ninja" = "Install Ninja for ISR computers."
                 "ISR Add Bookmarks" = "Add ISR bookmarks to Chrome."
-            }) -LineAfter
+            })
 
         if ($choice -eq 0) { $command = "nuvia install tscan" }
         if ($choice -eq 1) { $command = "nuvia isr onboard" }
@@ -22,7 +16,7 @@ function menu {
 
         read-command -command $command
     } catch {
-        exit-script -Type "error" -Text "Menu error: $($_.Exception.Message) $url/nuvia/$dependency.ps1" 
+        exit-script -Type "error" -Text "nuvia-menu: $($_.Exception.Message) $url/nuvia/$dependency.ps1" 
     }
 }
 
