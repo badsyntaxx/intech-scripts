d4b74fe50e67300b075610d28af4e9a1618677e7
badsyntaxx <badsyntaxx@gmail.com>
2024-06-11 14:40:30 -0600

    bug fix

--- intech/menu.ps1
+++ intech/menu.ps1
@@ -6,7 +6,7 @@ function menu {
             })
 
         if ($choice -eq 0) { $command = "intech add admin" }
-        if ($choice -eq 0) { $command = "nuvia menu" }
+        if ($choice -eq 1) { $command = "nuvia menu" }
 
         read-command -command $command
     } catch {
