df3a16313ca3e8022e74925c0c3d11daed200fba
badsyntaxx <badsyntaxx@gmail.com>
2024-06-17 14:44:35 -0600

    update

--- nuvia/isr-onboard.ps1
+++ nuvia/isr-onboard.ps1
@@ -72,20 +72,19 @@ function isr-install-ninja {
         
         if ($null -ne $service -and $service.Status -eq "Running") {
             write-text -type "plain" -Text "NinjaRMMAgent is already installed and running."
-            read-command
-        } 
-
-        $download = get-download -Url $Url -Target "$env:TEMP\NinjaOne.msi"
-        if (!$download) { throw "Unable to acquire intaller." }
+        } else {
+            $download = get-download -Url $Url -Target "$env:TEMP\NinjaOne.msi"
+            if (!$download) { throw "Unable to acquire intaller." }
           
-        Start-Process -FilePath "msiexec" -ArgumentList "/i `"$env:TEMP\NinjaOne.msi`" /qn" -Wait
+            Start-Process -FilePath "msiexec" -ArgumentList "/i `"$env:TEMP\NinjaOne.msi`" /qn" -Wait
 
-        $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
-        if ($null -eq $service -or $service.Status -ne "Running") { throw "NinjaOne did not successfully install." }
+            $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
+            if ($null -eq $service -or $service.Status -ne "Running") { throw "NinjaOne did not successfully install." }
 
-        Get-Item -ErrorAction SilentlyContinue "$env:TEMP\NinjaOne.msi" | Remove-Item -ErrorAction SilentlyContinue
+            Get-Item -ErrorAction SilentlyContinue "$env:TEMP\NinjaOne.msi" | Remove-Item -ErrorAction SilentlyContinue
 
-        write-text -type "success" -Text "NinjaOne successfully installed." -lineAfter
+            write-text -type "success" -Text "NinjaOne successfully installed." -lineAfter
+        }
     } catch {
         # Display error message and end the script
         exit-script -type "error" -Text "isr-install-ninja-$($_.InvocationInfo.ScriptLineNumber) - $($_.Exception.Message)"
