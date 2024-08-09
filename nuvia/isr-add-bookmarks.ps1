82d136a09abba7c6df6f9128c46cea02b5c84692
badsyntaxx <badsyntaxx@gmail.com>
2024-08-09 13:09:42 -0600

    Bugfix

--- nuvia/isr-add-bookmarks.ps1
+++ nuvia/isr-add-bookmarks.ps1
@@ -1,5 +1,6 @@
 function isr-add-bookmarks {
     try {
+        $user = select-user -CustomHeader "Select user to install apps for"
         $profiles = [ordered]@{}
         $chromeUserDataPath = "C:\Users\$($user["Name"])\AppData\Local\Google\Chrome\User Data"
         if (!(Test-Path $chromeUserDataPath)) {
