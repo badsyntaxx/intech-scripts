0c2e6dcfc8ae04cde577cf1cd36ae275ba030ed1
badsyntaxx <badsyntaxx@gmail.com>
2024-08-14 14:45:41 -0600

    Update

--- core/framework.ps1
+++ core/framework.ps1
@@ -405,7 +405,7 @@ function read-input {
         # Get current cursor position
         $currPos = $host.UI.RawUI.CursorPosition
 
-        Write-Host "  : " -NoNewline
+        Write-Host "  ? " -NoNewline
         if ($IsSecure) { $userInput = Read-Host -AsSecureString } 
         else { $userInput = Read-Host }
 
