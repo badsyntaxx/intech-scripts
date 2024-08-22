2e67ecf4e22359ae9cdbea1a444792d696004346
badsyntaxx <badsyntaxx@gmail.com>
2024-08-22 10:26:03 -0600

    Update

--- nuvia/isr-install-ninja.ps1
+++ nuvia/isr-install-ninja.ps1
@@ -1,9 +1,9 @@
 function isr-install-ninja {
     try {
-        $url = "https://app.ninjarmm.com/agent/installer/0274c0c3-3ec8-44fc-93cb-79e96f191e07/nuviaisrcenterremote-5.9.9652-windows-installer.msi"
+        $url = "https://app.ninjarmm.com/agent/installer/0274c0c3-3ec8-44fc-93cb-79e96f191e07/nuviaisrcenterremote-5.9.1158-windows-installer.msi"
         $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
 
-        write-text -type "notice" -text "nuviaisrcenterremote-5.9.9652-windows-installer.msi" -lineBefore
+        write-text -type "notice" -text "nuviaisrcenterremote-5.9.1158-windows-installer.msi" -lineBefore
         
         if ($null -ne $service -and $service.Status -eq "Running") {
             write-text -type "plain" -text "NinjaRMMAgent is already installed and running."
