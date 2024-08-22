31694066f92d54c4896292408f01874594fa8b4f
badsyntaxx <badsyntaxx@gmail.com>
2024-08-22 17:50:47 -0600

    Update

--- nuvia/isr-install-apps.ps1
+++ nuvia/isr-install-apps.ps1
@@ -262,14 +262,16 @@ function Install-Program {
             $dots = ""
             $counter = 0
             while (!$process.HasExited) {
+                $dotPos = $host.UI.RawUI.CursorPosition
                 $dots += "."
-                Write-Host -NoNewLine "`r  Installing$dots    "
+                Write-Host -NoNewLine "`r  Installing$dots"
                 Start-Sleep -Milliseconds 500
                 $counter++
                 if ($counter -eq 5) { 
                     $dots = "" 
                     $counter = 0
                 }
+                [Console]::SetCursorPosition($dotPos.X, $dotPos.Y)
             }
 
             [Console]::SetCursorPosition($curPos.X, $curPos.Y)
