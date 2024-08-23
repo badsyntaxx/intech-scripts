c95dc00bd7a40c43e539b6d12ae8b0be48c03ed6
badsyntaxx <badsyntaxx@gmail.com>
2024-08-23 12:52:47 -0600

    Update

--- nuvia/install-jumpcloud.ps1
+++ nuvia/install-jumpcloud.ps1
@@ -7,7 +7,6 @@ function install-jumpcloud {
             write-text -type "success" -text "JumpCloud Agent Already Installed."
             read-command
         } 
-        write-text "Downloading JCAgent Installer"
 
         $AGENT_INSTALLER_URL = "https://cdn02.jumpcloud.com/production/jcagent-msi-signed.msi"
         $AGENT_INSTALLER_PATH = "$env:SystemRoot\Temp\jcagent-msi-signed.msi"
@@ -16,27 +15,10 @@ function install-jumpcloud {
         if (!$download) { 
             throw "Unable to acquire intaller." 
         }
-
-        write-text "JumpCloud Agent Download Complete"
-        write-text "Running JCAgent Installer"
             
         $JumpCloudConnectKey = "fe8929df5bbccb8aceb58385b88aba034b7d69f7";
         msiexec /i $AGENT_INSTALLER_PATH /quiet JCINSTALLERARGUMENTS=`"-k $JumpCloudConnectKey /VERYSILENT /NORESTART /NOCLOSEAPPLICATIONS /L*V "C:\Windows\Temp\jcUpdate.log"`"
 
-        # Check if agent is running as a service
-        # Do a loop for 5 minutes to check if the agent is running as a service
-        # The agent pulls cef files during install which may take longer then previously.
-        <# for ($i = 0; $i -lt 300; $i++) {
-            Start-Sleep -Seconds 1
-            #Output the errors encountered
-            $AgentService = Get-Service -Name "jumpcloud-agent" -ErrorAction SilentlyContinue
-            if ($AgentService.Status -eq "Running") {
-                
-                break
-            } else {
-                write-text -type "error" -text "JumpCloud Agent Failed to Install"
-            }
-        } #>
         $curPos = $host.UI.RawUI.CursorPosition
         $dots = ""
         $counter = 0
