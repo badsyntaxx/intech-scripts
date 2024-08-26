c5817d563e8c925c121c3471a05ab4a8f040a067
badsyntaxx <badsyntaxx@gmail.com>
2024-08-26 14:22:30 -0600

    Update

--- core/framework.ps1
+++ core/framework.ps1
@@ -67,6 +67,8 @@ function read-command {
             read-command
         }
 
+        $lowercaseCommand = $command.ToLower()
+
         # Adjust command and paths
         $subCommands = @("plugins", "nuvia", "intech");
         $subPath = "windows"
@@ -75,13 +77,11 @@ function read-command {
                 $firstWord = "nuvia"
             }
             if ($firstWord -eq $sub -and $firstWord -ne 'menu') { 
-                $command = $command -replace "^$firstWord \s*", "" 
+                $lowercaseCommand = $lowercaseCommand -replace "^$firstWord \s*", "" 
                 $subPath = $sub
             }
         }
 
-        # Convert command to title case and replace the first spaces with a dash and the second space with no space
-        $lowercaseCommand = $command.ToLower()
         $fileFunc = $lowercaseCommand -replace ' ', '-'
 
         # Create the main script file
