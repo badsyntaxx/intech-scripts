377253af3830f292617e0c5e04cf96767de9fea5
badsyntaxx <badsyntaxx@gmail.com>
2024-08-30 20:09:01 -0600

    Update

--- nuvia/ISR Onboard.ps1
+++ nuvia/ISR Onboard.ps1
@@ -14,97 +14,53 @@ function isrOnboard {
     New-Item -Path "$env:SystemRoot\Temp\CHASTE-Script.ps1" -ItemType File -Force | Out-Null
 
     foreach ($func in $funcs) {
-        add-func -command $func
+        addFunc -command $func
     }
 
-    add-onboardScript -commandPath "core" -script "Framework"
+    addScript -directory "core" -script "Framework"
 
     Add-Content -Path "$env:SystemRoot\Temp\CHASTE-Script.ps1" -Value @"
-function run-all {
+function runAll {
     writeText -type "header" -text "Editing hostname" -lineBefore
-    edit-hostname
+    editHostname
     writeText -type "header" -text "Installing NinjaRMM" -lineBefore
-    install-ninja 
+    installNinja
     Write-Host
     writeText -type "header" -text "Installing JumpCloud" -lineBefore
-    install-jumpcloud 
+    installJumpCloud
     writeText -type "header" -text "Installing ISR apps" -lineBefore
     isrInstallApps
     writeText -type "header" -text "Adding InTech admin" -lineBefore
-    add-admin
+    addInTechAdmin
     writeText -type "header" -text "Installing BGInfo" -lineBefore
-    install-bginfo
+    installBGInfo
     writeText -type "header" -text "Disabling context menu" -lineBefore
-    toggle-context-menu
+    toggleContextMenu
     writeText -type "header" -text "Debloating Windows" -lineBefore
-    reclaimw11
+    reclaim
 }
 "@
     # Add a final line that will invoke the desired function
-    Add-Content -Path "$env:SystemRoot\Temp\CHASTE-Script.ps1" -Value "invokeScript 'run-all'"
+    Add-Content -Path "$env:SystemRoot\Temp\CHASTE-Script.ps1" -Value "invokeScript 'runAll'"
 
     # Execute the combined script
     $chasteScript = Get-Content -Path "$env:SystemRoot\Temp\CHASTE-Script.ps1" -Raw
     Invoke-Expression $chasteScript
 }
 
-function add-func {
+function addFunc {
     param (
         [Parameter(Mandatory = $false)]
         [string]$command = ""
     )
 
     try {
-        if ($command -ne "" -and $command -match "^(?-i)(\w+(-\w+)*)") { 
-            $firstWord = $matches[1] 
-        }
+        $filteredCommand = filterCommands -command $command
+        $commandDirectory = $filteredCommand[0]
+        $commandFile = $filteredCommand[1]
 
-        $commandPath = "windows"
-        $potentialPaths = @("plugins", "nuvia", "intech");
-
-        foreach ($pp in $potentialPaths) {
-            if ($firstWord -eq $pp -and $firstWord -ne 'menu') { 
-                $command = $command -replace "^$firstWord \s*", "" 
-                $commandPath = $pp
-            }
-        }
-
-        # Convert command to title case and replace the first spaces with a dash and the second space with no space
-        $lowercaseCommand = $command.ToLower()
-        $fileFunc = $lowercaseCommand -replace ' ', '-'
-
-        add-onboardScript -commandPath $commandPath -script $fileFunc
+        addScript -directory $commandDirectory -script $commandFile
     } catch {
-        # Error handling: display an error message and prompt for a new command
-        Write-Host "  $($_.Exception.Message) | init-$($_.InvocationInfo.ScriptLineNumber)" -ForegroundColor Red
-    }
-}
-
-function add-onboardScript {
-    param (
-        [Parameter(Mandatory)]
-        [string]$commandPath,
-        [Parameter(Mandatory)]
-        [string]$script,
-        [Parameter(Mandatory = $false)]
-        [string]$progressText
-    )
-
-    if ($commandPath -eq 'windows' -or $commandPath -eq 'plugins') {
-        $url = "https://raw.githubusercontent.com/badsyntaxx/chaste-scripts/main"
-    } else {
-        $url = "https://raw.githubusercontent.com/badsyntaxx/intech-scripts/main"
-    }
-
-    # Download the script
-    $download = getDownload -Url "$url/$commandPath/$script.ps1" -Target "$env:SystemRoot\Temp\$script.ps1" -failText "$url/$commandPath/$script | Could not acquire onboarding components."
-    
-    if ($download) { 
-        # Append the script to the main script
-        $rawScript = Get-Content -Path "$env:SystemRoot\Temp\$script.ps1" -Raw -ErrorAction SilentlyContinue
-        Add-Content -Path "$env:SystemRoot\Temp\CHASTE-Script.ps1" -Value $rawScript
-
-        # Remove the script file
-        Get-Item -ErrorAction SilentlyContinue "$env:SystemRoot\Temp\$script.ps1" | Remove-Item -ErrorAction SilentlyContinue
+        writeText -type "error" -text "addFunc-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)"
     }
 }
