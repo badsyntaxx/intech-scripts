9f940698dfbedf20b3fca2a9efa9a89c2c55861b
badsyntaxx <badsyntaxx@gmail.com>
2024-08-30 19:10:35 -0600

    Update

--- core/test.ps1
+++ core/test.ps1
@@ -1,3 +1,41 @@
+function intech {
+    Write-Host
+    Write-Host "  Try" -NoNewline
+    Write-Host " intech help" -ForegroundColor "Cyan" -NoNewline
+    Write-Host " or" -NoNewline
+    Write-Host " intech menu" -NoNewline -ForegroundColor "Cyan"
+    Write-Host " if you don't know what to do."
+}
+function readMenu {
+    try {
+        $choice = readOption -options $([ordered]@{
+                "Add InTechAdmin" = "Create the InTechAdmin local account."
+                "Nuvia"           = "View the Nuvia menu."
+                "Cancel"          = "Select nothing and exit this menu."
+            }) -prompt "Select an InTech function:"
+
+        switch ($choice) {
+            0 { $command = "intech add admin" }
+            1 { $command = "nuvia menu" }
+            2 { readCommand }
+        }
+
+        Write-Host
+        Write-Host ": "  -ForegroundColor "DarkCyan" -NoNewline
+        Write-Host "Running command:" -NoNewline -ForegroundColor "DarkGray"
+        Write-Host " $command" -ForegroundColor "Gray"
+
+        readCommand -command $command
+    } catch {
+        writeText -type "error" -text "intech-menu-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)" -lineAfter
+        readCommand
+    }
+}
+function writeHelp {
+    writeText -type "plain" -text "COMMANDS:"
+    writeText -type "plain" -text "intech add admin  - Create the InTech admin account." -Color "DarkGray"
+    writeText -type "plain" -text "schedule reboot   - Schedule a reboot for Wednesday at 10PM" -Color "DarkGray"
+}
 function invokeScript {
     param (
         [parameter(Mandatory = $true)]
@@ -50,8 +88,11 @@ function readCommand {
         $command = $command.Trim()
 
         if ($command -ne "help" -and $command -ne "" -and $command -match "^(?-i)(\w+(-\w+)*)") {
-            if (Get-command $matches[1] -ErrorAction SilentlyContinue) {
-                if ($matches[1] -ne "intech") {
+            $firstword = $matches[1]
+            if (Get-Command $firstword -ErrorAction SilentlyContinue) {
+ 
+                if ($firstword -ne "intech") {
+                    read-host "test"
                     Invoke-Expression $command
                     readCommand
                 }
@@ -699,3 +740,6 @@ function selectUser {
         writeText -type "error" -text "selectUser-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)"
     }
 }
+
+
+readCommand -command "intech menu"
