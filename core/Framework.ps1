7cfdf416332066004a090a79c55ad3540efc5ac6
badsyntaxx <badsyntaxx@gmail.com>
2024-09-03 17:29:11 -0600

    Update

--- core/Framework.ps1
+++ core/Framework.ps1
@@ -510,12 +510,9 @@ function getDownload {
             $progbar = $progbar.PadRight($curBarSize, [char]9608)
             $progbar = $progbar.PadRight($BarSize, [char]9617)
 
-            if (!$Complete.IsPresent) {
-                Write-Host -NoNewLine "`r    $ProgressText $progbar $($percentComplete.ToString("##0.00").PadLeft(6))%"
-            } else {
-                Write-Host -NoNewLine "`r    $ProgressText $progbar $($percentComplete.ToString("##0.00").PadLeft(6))%"                    
-            }              
-             
+            Write-Host -NoNewLine "`r  $ProgressText" -NoNewLine
+            Write-Host -NoNewLine " $progbar" -NoNewLine -ForegroundColor "Cyan"
+            Write-Host -NoNewLine " $($percentComplete.ToString("##0.00").PadLeft(6))%"            
         }
     }
     Process {
