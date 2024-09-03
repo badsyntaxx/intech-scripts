419b98c75f1cd88bd9ecab1eed7c5d6bd463de4d
badsyntaxx <badsyntaxx@gmail.com>
2024-09-03 17:22:38 -0600

    update

--- core/Framework.ps1
+++ core/Framework.ps1
@@ -478,9 +478,7 @@ function getDownload {
         [parameter(Mandatory = $false)]
         [int]$MaxRetries = 2,
         [parameter(Mandatory = $false)]
-        [int]$Interval = 1,
-        [parameter(Mandatory = $false)]
-        [switch]$visible = $false
+        [int]$Interval = 1
     )
     Begin {
         function Show-Progress {
@@ -571,22 +569,22 @@ function getDownload {
                     $total += $count
                     $totalMB = $total / 1024 / 1024
           
-                    if ($visible) {
-                        if ($fullSize -gt 0) {
-                            Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB"
-                        }
-
-                        if ($total -eq $fullSize -and $count -eq 0 -and $finalBarCount -eq 0) {
-                            Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB" -Complete
-                            $finalBarCount++
-                        }
+                    
+                    if ($fullSize -gt 0) {
+                        Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB"
                     }
+
+                    if ($total -eq $fullSize -and $count -eq 0 -and $finalBarCount -eq 0) {
+                        Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB" -Complete
+                        $finalBarCount++
+                    }
+                    
                 } while ($count -gt 0)
 
                 # Prevent the following output from appearing on the same line as the progress bar
-                if ($visible) {
-                    Write-Host 
-                }
+         
+                Write-Host 
+                
                 
                 if ($downloadComplete) { return $true } else { return $false }
             } catch {
