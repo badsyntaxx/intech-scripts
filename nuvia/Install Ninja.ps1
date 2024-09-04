16c7f1c71d054669ecb6dd39fe7b3503104b0d5f
badsyntaxx <badsyntaxx@gmail.com>
2024-09-04 12:09:41 -0600

    Update

--- nuvia/Install Ninja.ps1
+++ nuvia/Install Ninja.ps1
@@ -12,29 +12,30 @@ function installNinja {
         switch ($choice) {
             1 { $location = "nuviaisrcenteroremut" }
             2 { $location = "nuviaisrcenterremote" }
-            3 { readCommand }
         }
 
-        $url = "https://app.ninjarmm.com/agent/installer/0274c0c3-3ec8-44fc-93cb-79e96f191e07/$location-5.9.1158-windows-installer.msi"
-        $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
+        if ($choice -ne 3) {
+            $url = "https://app.ninjarmm.com/agent/installer/0274c0c3-3ec8-44fc-93cb-79e96f191e07/$location-5.9.1158-windows-installer.msi"
+            $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
 
-        writeText -type "notice" -text "$location-5.9.1158-windows-installer.msi"
+            writeText -type "notice" -text "$location-5.9.1158-windows-installer.msi"
         
-        if ($null -ne $service -and $service.Status -eq "Running") {
-            writeText -type "success" -text "NinjaRMMAgent is already installed and running."
-        } else {
-            $download = getDownload -url $url -target "$env:SystemRoot\Temp\NinjaOne.msi" 
-            if ($download) { 
-                Start-Process -FilePath "msiexec" -ArgumentList "/i `"$env:SystemRoot\Temp\NinjaOne.msi`" /qn" -Wait
+            if ($null -ne $service -and $service.Status -eq "Running") {
+                writeText -type "success" -text "NinjaRMMAgent is already installed and running."
+            } else {
+                $download = getDownload -url $url -target "$env:SystemRoot\Temp\NinjaOne.msi" 
+                if ($download) { 
+                    Start-Process -FilePath "msiexec" -ArgumentList "/i `"$env:SystemRoot\Temp\NinjaOne.msi`" /qn" -Wait
 
-                Start-Sleep -Seconds 3
+                    Start-Sleep -Seconds 3
 
-                $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
-                if ($null -eq $service -or $service.Status -ne "Running") { throw "NinjaOne did not successfully install." }
+                    $service = Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue
+                    if ($null -eq $service -or $service.Status -ne "Running") { throw "NinjaOne did not successfully install." }
 
-                Get-Item -ErrorAction SilentlyContinue "$env:SystemRoot\Temp\NinjaOne.msi" | Remove-Item -ErrorAction SilentlyContinue
+                    Get-Item -ErrorAction SilentlyContinue "$env:SystemRoot\Temp\NinjaOne.msi" | Remove-Item -ErrorAction SilentlyContinue
 
-                writeText -type "success" -text "NinjaOne successfully installed." -lineAfter
+                    writeText -type "success" -text "NinjaOne successfully installed." -lineAfter
+                }
             }
         }
     } catch {
