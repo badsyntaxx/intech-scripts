dc62290b3c6372ca6e384f0575ca8228ce7668d0
badsyntaxx <badsyntaxx@gmail.com>
2024-09-04 16:01:30 -0600

    Update

--- nuvia/Install JumpCloud.ps1
+++ nuvia/Install JumpCloud.ps1
@@ -3,42 +3,41 @@ function installJumpCloud {
         ipconfig /FlushDNS | Out-Null
         $AGENT_PATH = Join-Path ${env:ProgramFiles} "JumpCloud"
         $AGENT_BINARY_NAME = "jumpcloud-agent.exe"
-        if (Test-Path -Path "$($AGENT_PATH)\$($AGENT_BINARY_NAME)") {
-            writeText -type "success" -text "JumpCloud Agent Already Installed."
-            readCommand
-        } 
-
-        $AGENT_INSTALLER_url = "https://cdn02.jumpcloud.com/production/jcagent-msi-signed.msi"
-        $AGENT_INSTALLER_PATH = "$env:SystemRoot\Temp\jcagent-msi-signed.msi"
+        if (-not (Test-Path -Path "$($AGENT_PATH)\$($AGENT_BINARY_NAME)")) {
+            $AGENT_INSTALLER_url = "https://cdn02.jumpcloud.com/production/jcagent-msi-signed.msi"
+            $AGENT_INSTALLER_PATH = "$env:SystemRoot\Temp\jcagent-msi-signed.msi"
         
-        $download = getDownload -url $AGENT_INSTALLER_url -target $AGENT_INSTALLER_PATH -lineBefore
-        if ($download) {             
-            $JumpCloudConnectKey = "fe8929df5bbccb8aceb58385b88aba034b7d69f7";
-            msiexec /i $AGENT_INSTALLER_PATH /quiet JCINSTALLERARGUMENTS=`"-k $JumpCloudConnectKey /VERYSILENT /NORESTART /NOCLOSEAPPLICATIONS /L*V "C:\Windows\Temp\jcUpdate.log"`"
+            $download = getDownload -url $AGENT_INSTALLER_url -target $AGENT_INSTALLER_PATH -lineBefore
+            if ($download) {             
+                $JumpCloudConnectKey = "fe8929df5bbccb8aceb58385b88aba034b7d69f7";
+                msiexec /i $AGENT_INSTALLER_PATH /quiet JCINSTALLERARGUMENTS=`"-k $JumpCloudConnectKey /VERYSILENT /NORESTART /NOCLOSEAPPLICATIONS /L*V "C:\Windows\Temp\jcUpdate.log"`"
 
-            $curPos = $host.UI.RawUI.CursorPosition
+                $curPos = $host.UI.RawUI.CursorPosition
 
-            while (!$process.HasExited) {
-                $AgentService = Get-Service -Name "jumpcloud-agent" -ErrorAction SilentlyContinue
-                if ($AgentService.Status -eq "Running") {
-                    writeText -type "success" -text "JumpCloud Agent Installed."
-                    break
-                } else {
-                    Write-Host -NoNewLine "`r  Installing |"
-                    Start-Sleep -Milliseconds 150
-                    Write-Host -NoNewLine "`r  Installing /"
-                    Start-Sleep -Milliseconds 150
-                    Write-Host -NoNewLine "`r  Installing $([char]0x2015)"
-                    Start-Sleep -Milliseconds 150
-                    Write-Host -NoNewLine "`r  Installing \"
-                    Start-Sleep -Milliseconds 150
+                while (!$process.HasExited) {
+                    $AgentService = Get-Service -Name "jumpcloud-agent" -ErrorAction SilentlyContinue
+                    if ($AgentService.Status -eq "Running") {
+                        writeText -type "success" -text "JumpCloud Agent Installed."
+                        break
+                    } else {
+                        Write-Host -NoNewLine "`r  Installing |"
+                        Start-Sleep -Milliseconds 150
+                        Write-Host -NoNewLine "`r  Installing /"
+                        Start-Sleep -Milliseconds 150
+                        Write-Host -NoNewLine "`r  Installing $([char]0x2015)"
+                        Start-Sleep -Milliseconds 150
+                        Write-Host -NoNewLine "`r  Installing \"
+                        Start-Sleep -Milliseconds 150
+                    }
                 }
+
+                [Console]::SetCursorPosition($curPos.X, $curPos.Y)
+
+                Write-Host "                                                     `r"
             }
-
-            [Console]::SetCursorPosition($curPos.X, $curPos.Y)
-
-            Write-Host "                                                     `r"
         }
+        
+        writeText -type "success" -text "JumpCloud Agent Already Installed."
     } catch {
         writeText -type "error" -text "installJumpCloud-$($_.InvocationInfo.ScriptLineNumber) - $($_.Exception.Message)"
     }
