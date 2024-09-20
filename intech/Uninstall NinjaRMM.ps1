abfe68eb6306ba4a41052d1dbefe29d664ee7992
badsyntaxx <badsyntaxx@gmail.com>
2024-09-20 09:50:58 -0600

    Update

--- intech/Uninstall NinjaRMM.ps1
+++ intech/Uninstall NinjaRMM.ps1
@@ -2,67 +2,69 @@ function uninstallNinjaRMM {
     $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $p = New-Object System.Security.Principal.WindowsPrincipal($id)
     if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { 
-        Write-Host "[x] Searching for NinjaRMMAgent(s)" -Fore Cyan
-        Write-Host "[-] Searching in $($env:ProgramFiles)"
+        writeText -type "plain" -text "[x] Searching for NinjaRMMAgent(s)"
+        writeText -type "plain" -text "Searching in $($env:ProgramFiles)"
         $NinjaExe = ""
         $folders = Get-ChildItem "$($env:ProgramFiles)"
-        foreach ( $folder in $folders ){
-            if ( Test-Path -Path "$($env:ProgramFiles)\$($folder)" ){
-                if ( Test-Path -Path "$($env:ProgramFiles)\$($folder)\NinjaRMMAgent.exe" ){
-                    Write-Host "[-] Found NinjaRMMAgent at $($env:ProgramFiles)\$($folder)\NinjaRMMAgent.exe" -Fore Yellow
+        foreach ($folder in $folders) {
+            if (Test-Path -Path "$($env:ProgramFiles)\$($folder)") {
+                if (Test-Path -Path "$($env:ProgramFiles)\$($folder)\NinjaRMMAgent.exe") {
+                    writeText -type "notice" -text "Found NinjaRMMAgent at $($env:ProgramFiles)\$($folder)\NinjaRMMAgent.exe"
                     $NinjaExe = "$($env:ProgramFiles)\$($folder)\NinjaRMMAgent.exe";
                 }
             }
         }
-        Write-Host "[-] Searching in $(${env:ProgramFiles(x86)})"
+        writeText -type "plain" -text "Searching in $(${env:ProgramFiles(x86)})"
         $folders = Get-ChildItem "$(${env:ProgramFiles(x86)})"
-        foreach ( $folder in $folders ){
-            if ( Test-Path -Path "$(${env:ProgramFiles(x86)})\$($folder)" ){
-                if ( Test-Path -Path "$(${env:ProgramFiles(x86)})\$($folder)\NinjaRMMAgent.exe" ){
-                    Write-Host "[-] Found NinjaRMMAgent at $(${env:ProgramFiles(x86)})\$($folder)\NinjaRMMAgent.exe" -Fore Yellow
+        foreach ($folder in $folders) {
+            if (Test-Path -Path "$(${env:ProgramFiles(x86)})\$($folder)") {
+                if (Test-Path -Path "$(${env:ProgramFiles(x86)})\$($folder)\NinjaRMMAgent.exe") {
+                    writeText -type "notice" -text "Found NinjaRMMAgent at $(${env:ProgramFiles(x86)})\$($folder)\NinjaRMMAgent.exe"
                     $NinjaExe = "$(${env:ProgramFiles(x86)})\$($folder)\NinjaRMMAgent.exe";
                 }
             }
         }
     
-        if ( $NinjaExe -ne "" ){
-            Write-Host "[-] Ninja Agent was Found, Continuing." -Fore Green
-            Write-Host "[-] Validating the Ninja Agent Service is Stopped."
+        if ($NinjaExe -ne "") {
+            writeText -type "success" -text "Ninja Agent was Found, Continuing." 
+            writeText -type "plain" -text "Validating the Ninja Agent Service is Stopped."
             Stop-Service -Name NinjaRMMAgent -Force
             $service = Get-Service -Name NinjaRMMAgent
-            While ( $service.Status -eq "Running" ){
+            While ($service.Status -eq "Running") {
                 $service = Get-Service -Name NinjaRMMAgent
-                Write-Host "[o] Waiting for NinjaRMMAgent Service to Stop." -Fore Yellow
+                writeText -type "notice" -text "[o] Waiting for NinjaRMMAgent Service to Stop."
             }
-            Write-Host "[-] Executing $($NinjaExe) --disableUninstallPrevention"
+            writeText -type "plain" -text "Executing $($NinjaExe) --disableUninstallPrevention"
             $process = Start-Process -FilePath "$($NinjaExe)" -ArgumentList "--disableUninstallPrevention" -Wait -PassThru -NoNewWindow
-            Write-Host "[-] Process exited with Exit Code: $($process.ExitCode)"
-            if ( $process.ExitCode -eq 0 ){
-                Write-Host "[-] Successfully disabled Uninstall Prevention." -Fore Green
-                Write-Host "[-] Checking for Uninstaller."
+            writeText -type "plain" -text "Process exited with Exit Code: $($process.ExitCode)"
+            if ($process.ExitCode -eq 0) {
+                writeText -type "success" -text "Successfully disabled Uninstall Prevention." 
+                writeText -type "plain" -text "Checking for Uninstaller."
                 $Uninstaller = $NinjaExe.Replace("NinjaRMMAgent.exe", "uninstall.exe")
-                if ( Test-Path -Path $Uninstaller ){
-                    Write-Host "[-] Uninstaller Exists, Continuing." -Fore Yellow
+                if (Test-Path -Path $Uninstaller) {
+                    writeText -type "notice" -text "Uninstaller Exists, Continuing."
                     $process = Start-Process -FilePath $Uninstaller -ArgumentList "--mode unattended" -Wait -PassThru -NoNewWindow
-                    Write-Host "[-] Uninstaller Exited with Code: $($process.ExitCode)"
-                    if ( $process.ExitCode -eq 0 ){
-                        Write-Host "[-] Uninstall was successful. Performing Cleanup." -Fore Green
+                    writeText -type "plain" -text "Uninstaller Exited with Code: $($process.ExitCode)"
+                    if ($process.ExitCode -eq 0) {
+                        writeText -type "success" -text "Uninstall was successful. Performing Cleanup." 
                         $NinjaDirectory = $NinjaExe.Replace("NinjaRMMAgent.exe", "")
-                        Write-Host "[-] Removing $($NinjaDirectory)"
+                        writeText -type "plain" -text "Removing $($NinjaDirectory)"
                         Remove-Item $NinjaDirectory -Force -Recurse -ErrorAction SilentlyContinue
-                        Write-Host "[-] Removing $($env:ProgramData)\NinjaRMMAgent\"
+                        writeText -type "plain" -text "Removing $($env:ProgramData)\NinjaRMMAgent\"
                         Remove-Item "$($env:ProgramData)\NinjaRMMAgent\" -Force -Recurse -ErrorAction SilentlyContinue
                     } else {
-                        Write-Host "[!] Uninstall failed." -Fore Red
+                        writeText -type "error" -text "Uninstall failed."
                     }
+                } else {
+                    writeText -type "error" -text "Cant find uninstaller."
                 }
             } else {
-                Write-Host "[-] Couldn't disable Uninstall Prevention. Make sure the service actually stopped running." -Fore Red
+                writeText -type "error" -text "Couldn't disable Uninstall Prevention. Make sure the service actually stopped running."
             }
         } else {
-            Write-Host "[!] Couldn't Find the Ninja Agent." -Fore Red
+            writeText -type "error" -text "Couldn't Find the Ninja Agent."
         }
     } else {
-        Write-Host "[!] This script must be ran as an admin." -Fore Red
+        writeText -type "error" -text "This script must be ran as an admin."
     }
 }
